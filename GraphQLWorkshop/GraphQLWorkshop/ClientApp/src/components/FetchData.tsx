65a2260f3a96e582a07ce1ead9c78c52f474bb1b
Chris Nissen <chris.nissen@ualberta.net>
2019-08-13 17:23:09 -0600

    Receiving one batch of forecasts for display on the client.

--- GraphQLWorkshop/GraphQLWorkshop/ClientApp/src/components/FetchData.tsx
+++ GraphQLWorkshop/GraphQLWorkshop/ClientApp/src/components/FetchData.tsx
@@ -8,64 +8,65 @@ import { ApplicationState } from '../store/configureStore';
 import { Input, Button, Alert } from 'reactstrap';
 import _ from 'lodash';
 import { Forecast } from 'models';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
 
 type Props = WeatherForecastsState
     & typeof actionCreators
     & RouteComponentProps<{ startDateIndex: string }>;
 
-class FetchData extends Component<Props> {
-    componentDidMount() {
-        // This method is called when the component is first added to the document
-        this.ensureDataFetched();
-    }
+const GetForecasts = gql`
+        query {
+            weatherForecasts {
+                id
+                date
+                dateFormatted
+                temperatureC
+                temperatureF
+                summary
+            }
+        }
+`;
 
-    componentDidUpdate() {
-        // This method is called when the route parameters change
-        this.ensureDataFetched();
-    }
+const FetchData: React.FunctionComponent<Props> = props => {
+    const { loading, error, data } = useQuery(GetForecasts);
 
-    ensureDataFetched() {
-        const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
-        this.props.requestWeatherForecasts(startDateIndex);
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
-                {renderForecastsTable(this.props)}
-                {renderPagination(this.props)}
-                {this.props.isLoading ? <Alert color="info">Loading...</Alert> : null}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Weather forecast</h1>
+            <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
+            {renderForecastsTable(props, data ? data.weatherForecasts : [])}
+            {renderPagination(props)}
+            {loading ? <Alert color="info">Loading...</Alert> : null}
+            {error ? <Alert color="danger">{`Error! ${error.message}`}</Alert> : null}
+        </div>
+    );
 }
 
-function renderForecastsTable(props: Props) {
-    const sortedForecasts = _.orderBy(props.forecasts, (f: Forecast) => f.date);
+function renderForecastsTable(props: Props, forecasts: Forecast[]) {
+    const sortedForecasts = _.orderBy(forecasts, (f: Forecast) => f.date);
     return (
         <table className='table table-striped'>
             <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
+            <tr>
+                <th>Date</th>
+                <th>Temp. (C)</th>
+                <th>Temp. (F)</th>
+                <th>Summary</th>
+            </tr>
             </thead>
             <tbody>
-                {sortedForecasts.map(forecast =>
-                    <tr key={forecast.dateFormatted}>
-                        <td>{forecast.dateFormatted}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>
-                            <Input value={forecast.summary}
-                                onChange={e => props.updateWeatherForecast(forecast.id, e.target.value)} />
-                        </td>
-                    </tr>
-                )}
+            {sortedForecasts.map(forecast =>
+                <tr key={forecast.dateFormatted}>
+                    <td>{forecast.dateFormatted}</td>
+                    <td>{forecast.temperatureC}</td>
+                    <td>{forecast.temperatureF}</td>
+                    <td>
+                        <Input value={forecast.summary}
+                               onChange={e => props.updateWeatherForecast(forecast.id, e.target.value)}/>
+                    </td>
+                </tr>
+            )}
             </tbody>
         </table>
     );
@@ -76,9 +77,9 @@ function renderPagination(props: Props) {
     const nextStartDateIndex = (props.startDateIndex || 0) + 5;
 
     return <p className='clearfix text-center'>
-        <Link className='btn btn-default pull-left' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
-        <Link className='btn btn-default pull-right' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
-    </p>;
+               <Link className='btn btn-default pull-left' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
+               <Link className='btn btn-default pull-right' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
+           </p>;
 }
 
 export default connect(
