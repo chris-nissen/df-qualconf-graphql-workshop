2ce8333441ce3e0f8c1310c5275618addae72192
Chris Nissen <chris.nissen@ualberta.net>
2019-08-19 21:20:06 -0600

    A little more cleanup.

--- GraphQLWorkshop/GraphQLWorkshop/ClientApp/src/index.tsx
+++ GraphQLWorkshop/GraphQLWorkshop/ClientApp/src/index.tsx
@@ -1,16 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserHistory } from 'history';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import ApolloClient from 'apollo-boost';
-
-// Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const history = createBrowserHistory({ basename: (baseUrl as string | undefined) });
 
 const apolloClient = new ApolloClient({
     resolvers: {}
